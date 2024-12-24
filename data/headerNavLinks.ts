364394145950484c7eb0a8f02afe539c9a3a3afb
dxx <dongxuxu.cool@163.com>
2024-12-25 01:10:59 +0800

    fix: yarn dev error

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -1,9 +1,16 @@
+// const headerNavLinks = [
+//   { href: '/', title: 'Home' },
+//   { href: '/blog', title: 'Blog' },
+//   { href: '/tags', title: 'Tags' },
+//   { href: '/projects', title: 'Projects' },
+//   { href: '/about', title: 'About' },
+// ]
 const headerNavLinks = [
-  { href: '/', title: 'Home' },
-  { href: '/blog', title: 'Blog' },
-  { href: '/tags', title: 'Tags' },
-  { href: '/projects', title: 'Projects' },
-  { href: '/about', title: 'About' },
+  { href: '/', title: '首页' },
+  { href: '/blog', title: '博客' },
+  { href: '/tags', title: '分类标签' },
+  { href: '/projects', title: '项目与产品' },
+  { href: '/about', title: '关于' },
 ]
 
 export default headerNavLinks
